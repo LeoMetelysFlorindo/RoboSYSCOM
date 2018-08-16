97366ab2349a20ea5706348362c2276120c2e0ab
Leo Metelys <leo.metelys@gmail.com>
2018-08-16 10:36:40 -0400

    RoboSiscom
    Aplicativo em Visual Studio 2015 na linguagem VB.NET para realizar o backup do aplicativo e do banco de dados para um servidor externo à aplicação em outro prédio

---
+++ RoboBackup/sRoboBackup/sRoboBackup/Form1.Designer.vb
@@ -0,0 +1,212 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TxtDataFinal = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TxtDataInicial = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.LbRegistros = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.Red
+        Me.Label5.Location = New System.Drawing.Point(13, 121)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(151, 16)
+        Me.Label5.TabIndex = 39
+        Me.Label5.Text = "Arquivos em Cópia..."
+        Me.Label5.Visible = False
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Red
+        Me.Label4.Location = New System.Drawing.Point(61, 80)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(246, 20)
+        Me.Label4.TabIndex = 38
+        Me.Label4.Text = "para os arquivos do SYSCOM"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.Red
+        Me.Label3.Location = New System.Drawing.Point(59, 60)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(256, 20)
+        Me.Label3.TabIndex = 37
+        Me.Label3.Text = "Backup automáticos de dados "
+        '
+        'TxtDataFinal
+        '
+        Me.TxtDataFinal.Location = New System.Drawing.Point(259, 20)
+        Me.TxtDataFinal.Name = "TxtDataFinal"
+        Me.TxtDataFinal.Size = New System.Drawing.Size(100, 20)
+        Me.TxtDataFinal.TabIndex = 30
+        Me.TxtDataFinal.Text = "31/10/2017"
+        Me.TxtDataFinal.Visible = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(195, 23)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(63, 13)
+        Me.Label2.TabIndex = 35
+        Me.Label2.Text = "Data Início:"
+        Me.Label2.Visible = False
+        '
+        'TxtDataInicial
+        '
+        Me.TxtDataInicial.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtDataInicial.Location = New System.Drawing.Point(89, 20)
+        Me.TxtDataInicial.Name = "TxtDataInicial"
+        Me.TxtDataInicial.Size = New System.Drawing.Size(100, 20)
+        Me.TxtDataInicial.TabIndex = 29
+        Me.TxtDataInicial.Text = "01/10/2017"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(20, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(70, 13)
+        Me.Label1.TabIndex = 33
+        Me.Label1.Text = "Data Backup"
+        '
+        'LbRegistros
+        '
+        Me.LbRegistros.AutoSize = True
+        Me.LbRegistros.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LbRegistros.ForeColor = System.Drawing.Color.Red
+        Me.LbRegistros.Location = New System.Drawing.Point(13, 145)
+        Me.LbRegistros.Name = "LbRegistros"
+        Me.LbRegistros.Size = New System.Drawing.Size(176, 20)
+        Me.LbRegistros.TabIndex = 32
+        Me.LbRegistros.Text = "Registro Atualização"
+        Me.LbRegistros.Visible = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(446, 95)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(130, 23)
+        Me.Button2.TabIndex = 34
+        Me.Button2.Text = "Finalizar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(446, 57)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(130, 23)
+        Me.Button1.TabIndex = 31
+        Me.Button1.Text = "Fazer Backup"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 15000
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 10000
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "NotifyIcon1"
+        Me.NotifyIcon1.Visible = True
+        '
+        'Timer3
+        '
+        Me.Timer3.Interval = 5000
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.sRoboBackup.My.Resources.Resources.SISCOM
+        Me.PictureBox1.Location = New System.Drawing.Point(475, 145)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(61, 60)
+        Me.PictureBox1.TabIndex = 40
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(607, 260)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TxtDataFinal)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TxtDataInicial)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.LbRegistros)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Backup Automático Diário  de Dados. 15/08/2018. Versão A01 BETA"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TxtDataFinal As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TxtDataInicial As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents LbRegistros As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+    Friend WithEvents Timer3 As Timer
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ RoboBackup/sRoboBackup/sRoboBackup/Form1.vb
@@ -0,0 +1,251 @@
+﻿' DATA: 15/08/2018 09:38h
+' Backup automático de dados
+' Aplicativo criado para realizar o Backup Automático de dados 
+' do sistema da Diretoria de Saúde
+' Desenvolvedor: Leonardo Metelys - ALEAM
+Imports System.Data.SqlClient
+Imports System.IO
+Imports System.Net.Mail.SmtpClient
+Imports System.Net.Mime
+Imports System.Data.Odbc
+Imports System.Data
+Imports System.Data.OleDb
+Imports System.Net.Mail
+Imports System.Net
+
+Public Class Form1
+
+    Public sDataAtual As String = Today
+    Public HoraExecucao1 As String = "07:00"
+    Public HoraExecucao2 As String = "14:30"
+    Public HoraExecucao3 As String = "09:30"
+    Public HoraExecucao4 As String = "00:00"
+    Public sMudaDataDia As Boolean = False
+    Dim CaminhoCopiaemUso As String = ""
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        End
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        sMudaDataDia = False
+
+        'Inicializar o Timer
+        Timer2.Enabled = True
+
+        Dim sData As String = Mid(Now, 1, 10)
+        TxtDataInicial.Text = "01/" + Mid(sData, 4, 7)
+        TxtDataFinal.Text = "01/" + Mid(sData, 4, 7)
+
+        TxtDataInicial.Enabled = False
+
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+
+        'Colocar na bandeja de tarefas
+        If Me.WindowState = FormWindowState.Normal Then
+
+            'NotifyIcon1.Visible = False
+            'Me.WindowState = FormWindowState.Minimized
+            Me.Hide()
+
+        End If
+
+        'Habilitar a Atualização
+        Timer1.Enabled = True
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+
+        Dim sDataHoje As String = ""
+        Dim sDataHoraHoje As DateTime = Now
+        Dim sHora As String = Mid(sDataHoraHoje, 12, 5)  'Pegar a Hora Atual
+        Dim sData As String = Mid(sDataHoraHoje, 1, 10)
+
+        sDataHoje = sDataAtual
+
+        'Executar a Atualização
+        'Me.WindowState = FormWindowState.Normal
+        'Me.Visible = True
+
+
+        'A cada 30segudos checar se atingiu a data e a hora determinadas para as três execuções diárias
+        If sDataAtual = sData And sHora = HoraExecucao1 Then
+
+            'Atualização iniciada
+            Timer1.Enabled = False
+
+            Timer2.Enabled = False
+
+            'Rodar a atualização
+            Timer3.Enabled = True
+
+        End If
+
+        'Backup da tarde
+        If sDataAtual = sData And sHora = HoraExecucao2 Then
+
+            'Atualização iniciada
+            Timer1.Enabled = False
+
+            Timer2.Enabled = False
+
+            'Rodar a atualização
+            Timer3.Enabled = True
+
+        End If
+
+
+        'Se é meia-noite é preciso alterar a data para a data do novo dia 
+        If sDataAtual <> sData And sHora = HoraExecucao4 Then
+
+            'Deve entrar aqui somente uma vez à meia-noite, porque após a variável sDataAtual ser atualizada
+            'nucam será diferente de sData
+            sMudaDataDia = True
+
+        End If
+
+        If sMudaDataDia = True Then
+            'Atualizar a data Atual
+            sDataAtual = Today
+
+        End If
+
+    End Sub
+
+    Sub copiarDiretorio(ByVal CaminhoFonte As String, ByVal CaminhoDestino As String, Optional ByVal Sobrepor As Boolean = False)
+
+        Dim DiretorioFonte As DirectoryInfo = New DirectoryInfo(CaminhoFonte)
+        Dim DiretorioDestino As DirectoryInfo = New DirectoryInfo(CaminhoDestino)
+
+
+        If DiretorioFonte.Exists Then
+
+            If Not DiretorioDestino.Parent.Exists Then
+                LbRegistros.Text = " O diretório de destino não existe : " + DiretorioDestino.FullName
+                LbRegistros.Visible = True
+            End If
+
+            Application.DoEvents()
+
+            'copia todos os arquivos do diretório
+            Dim arquivos As FileInfo
+
+            For Each arquivos In DiretorioFonte.GetFiles()
+
+                If Sobrepor Then
+                    arquivos.CopyTo(Path.Combine(DiretorioDestino.FullName, arquivos.Name), True)
+                Else
+
+                    If Not File.Exists(Path.Combine(DiretorioDestino.FullName, arquivos.Name)) Then
+
+                        arquivos.CopyTo(Path.Combine(DiretorioDestino.FullName, arquivos.Name), False)
+
+                        Label5.Text = " Arquivo " + arquivos.Name + " copiando... "
+                        Label5.Visible = True
+                        Application.DoEvents()
+
+
+                    End If
+
+                End If
+
+            Next
+
+            'copia todos os subdiretorios usando recursao
+            Dim subdir As DirectoryInfo
+            For Each subdir In DiretorioFonte.GetDirectories()
+
+                'Criar pasta em diretório
+                System.IO.Directory.CreateDirectory(CaminhoCopiaemUso + "\" + subdir.Name)
+                copiarDiretorio(subdir.FullName, Path.Combine(DiretorioDestino.FullName, subdir.Name), False)
+
+                Label5.Text = " Diretório " + subdir.Name + " copiando... "
+                Label5.Visible = True
+                Application.DoEvents()
+
+            Next
+
+        Else
+
+            LbRegistros.Text = " Diretório origem não existe : " + DiretorioFonte.FullName
+            LbRegistros.Visible = True
+
+            'Throw New DirectoryNotFoundException("Diretório origem não existe " + DiretorioFonte.FullName)
+
+        End If
+
+    End Sub
+
+    Private Sub Timer3_Tick(sender As Object, e As EventArgs) Handles Timer3.Tick
+
+        'Desabilitar o Timer
+        Timer3.Enabled = False
+
+        Me.Show()
+
+        Application.DoEvents()
+
+        Dim sData As String = Mid(Now, 1, 10)
+        Dim sHora As String = Mid(Now, 12, 5)  'Pegar a Hora Atual
+        Dim sHoraFile = Replace(sHora, ":", "_")
+        Dim CaminhoCopia = "C:\Backup\" + "SYSCOM_" + Mid(sData, 1, 2) + Mid(sData, 4, 2) + Mid(sData, 7, 4) + "_" + sHoraFile + "h"
+
+        'Criar pasta em diretório
+        System.IO.Directory.CreateDirectory(CaminhoCopia)
+        CaminhoCopiaemUso = CaminhoCopia
+
+        copiarDiretorio("\\172.16.4.71\clinicas4.0\", CaminhoCopia, False)
+
+        Application.DoEvents()
+
+        LbRegistros.Text = "Última Atualização em " + sDataAtual + " às " + sHora
+        LbRegistros.Visible = True
+
+        Application.DoEvents()
+
+        Timer2.Interval = 20000  'Aguarda o tempo especificado para esconder a tela novamente
+
+        'Habilitar novamente o Atualizador 
+        Timer2.Enabled = True
+
+    End Sub
+    'Botão para ativar Backup automaticamente
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        'Desabilitar o Timer
+        Timer3.Enabled = False
+
+        Me.Show()
+
+        Application.DoEvents()
+
+        Dim sData As String = Mid(Now, 1, 10)
+        Dim sHora As String = Mid(Now, 12, 5)  'Pegar a Hora Atual
+        Dim sHoraFile = Replace(sHora, ":", "_")
+        Dim CaminhoCopia = "C:\Backup\" + "SYSCOM_" + Mid(sData, 1, 2) + Mid(sData, 4, 2) + Mid(sData, 7, 4) + "_" + sHoraFile + "h"
+
+        'Criar pasta em diretório
+        System.IO.Directory.CreateDirectory(CaminhoCopia)
+        CaminhoCopiaemUso = CaminhoCopia
+
+
+        copiarDiretorio("\\172.16.4.71\clinicas4.0\", CaminhoCopia, False)
+
+        Application.DoEvents()
+
+        LbRegistros.Text = "Última Atualização em " + sDataAtual + " às " + sHora
+        LbRegistros.Visible = True
+
+        Application.DoEvents()
+
+        Timer2.Interval = 20000  'Aguarda o tempo especificado para esconder a tela novamente
+
+        'Habilitar novamente o Atualizador 
+        Timer2.Enabled = True
+
+    End Sub
+End Class
